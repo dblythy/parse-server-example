a80be8e100d9bd4a6eaef727649f2c57e0078630
dblythy <daniel-blyth@live.com.au>
2021-08-05 02:10:37 +1000

    add .env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
-
+require('dotenv').config({ path: `${__dirname}/.env` });
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
 const path = require('path');
